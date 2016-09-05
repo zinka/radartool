9846a97faa46ea8f0aff2ebe034c935cbca948e9
zinka <srinivas.zinka@gmail.com>
2016-09-05 11:25:02 +0530

    nothing

---
+++ radar.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+# Author: Srinivasa Rao Zinka (srinivas . zinka [at] gmail . com)
+# Copyright (c) 2015 Srinivasa Rao Zinka
+# License: New BSD License.
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+from mayavi import mlab
+
+# TODO range equation implementation for seach, track, etc
+# TODO PD, Pfa, Blake chart ... get more idead from Mahafza
+# Ambiguity fun using quad and inf boundaries
+# TODO implement Peebles book too
