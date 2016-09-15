b211cad1c04f32ff834925669821aa23cbfce1db
zinka <srinivas.zinka@gmail.com>
2016-09-15 08:12:31 +0530

    Name change

---
+++ radartool.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+# Author: Srinivasa Rao Zinka (srinivas . zinka [at] gmail . com)
+# Copyright (c) 2015 Srinivasa Rao Zinka
+# License: New BSD License.
+
+from __future__ import division
+import numpy as np
+import matplotlib.pyplot as plt
+from mayavi import mlab
+
+# TODO range equation implementation for seach, track, etc Mahafza, Eddy, Peebles ... Use IIR, Loss factor notations
+
+# Ambiguity fun using quad and inf boundaries for various waveforms
+
+# TODO PD, Pfa,  ... for various types of echos and Swerling models
+
+# TODO Blake chart
+
